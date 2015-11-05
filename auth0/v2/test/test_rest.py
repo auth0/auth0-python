9db8f22397279c76eb64c316cd1858e882249c5e
Juan Cabrera <jjcp.91@gmail.com>
2015-11-05 00:04:43 -0300

    Add test cases for RestClient.post()

--- auth0/v2/test/test_rest.py
+++ auth0/v2/test/test_rest.py
@@ -1,4 +1,5 @@
 import unittest
+import json
 import mock
 from ..rest import RestClient
 from ..exceptions import Auth0Error
@@ -50,6 +51,43 @@ class TestRest(unittest.TestCase):
 
         with self.assertRaises(Auth0Error) as context:
             rc.get()
+
+        self.assertEqual(context.exception.status_code, 999)
+        self.assertEqual(context.exception.error_code, 'code')
+        self.assertEqual(context.exception.message, 'message')
+
+    @mock.patch('requests.post')
+    def test_post(self, mock_post):
+        rc = RestClient(endpoint='the-url', jwt='a-token')
+        headers = {'Authorization': 'Bearer a-token',
+                   'Content-Type': 'application/json'}
+
+        mock_post.return_value.text = '["a", "b"]'
+
+        data = {'some': 'data'}
+
+        response = rc.post(data=data)
+        mock_post.assert_called_with('the-url', data=json.dumps(data),
+                                     headers=headers)
+
+        self.assertEqual(response, ['a', 'b'])
+
+        response = rc.post(data=data, id='ID')
+        mock_post.assert_called_with('the-url/ID', data=json.dumps(data),
+                                     headers=headers)
+
+        self.assertEqual(response, ['a', 'b'])
+
+    @mock.patch('requests.post')
+    def test_post_errors(self, mock_post):
+        rc = RestClient(endpoint='the-url', jwt='a-token')
+
+        mock_post.return_value.text = '{"statusCode": 999,' \
+                                      ' "errorCode": "code",' \
+                                      ' "message": "message"}'
+
+        with self.assertRaises(Auth0Error) as context:
+            rc.post()
 
         self.assertEqual(context.exception.status_code, 999)
         self.assertEqual(context.exception.error_code, 'code')
