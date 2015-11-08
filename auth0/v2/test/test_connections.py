e980316e0c9b6026962cf15e5c9abc19af72c287
Juan Cabrera <jjcp.91@gmail.com>
2015-11-08 12:10:07 -0300

    Add test_get() test case

--- auth0/v2/test/test_connections.py
+++ auth0/v2/test/test_connections.py
@@ -37,3 +37,30 @@ class TestConnection(unittest.TestCase):
         self.assertEqual(kwargs['params'], {'fields': 'a,b',
                                             'strategy': 'strategy',
                                             'include_fields': 'true'})
+
+    @mock.patch('auth0.v2.connection.RestClient')
+    def test_get(self, mock_rc):
+        mock_instance = mock_rc.return_value
+        mock_instance.get.return_value = {}
+
+        c = Connection(domain='domain', jwt_token='jwttoken')
+        c.get('an-id')
+
+        args, kwargs = mock_instance.get.call_args
+        self.assertEqual('https://domain/api/v2/connections/an-id', args[0])
+        self.assertEqual(kwargs['params'], {'fields': None,
+                                            'include_fields': 'true'})
+
+        c.get('an-id', fields=['a', 'b'])
+
+        args, kwargs = mock_instance.get.call_args
+        self.assertEqual('https://domain/api/v2/connections/an-id', args[0])
+        self.assertEqual(kwargs['params'], {'fields': 'a,b',
+                                            'include_fields': 'true'})
+
+        c.get('an-id', fields=['a', 'b'], include_fields=False)
+
+        args, kwargs = mock_instance.get.call_args
+        self.assertEqual('https://domain/api/v2/connections/an-id', args[0])
+        self.assertEqual(kwargs['params'], {'fields': 'a,b',
+                                            'include_fields': 'false'})
