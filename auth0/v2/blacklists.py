aed9b3066f9d796e5c89e38d833c87e130a421c3
Juan Cabrera <jjcp.91@gmail.com>
2015-11-11 22:10:38 -0300

    Fix Blacklists usage of RestClient

--- auth0/v2/blacklists.py
+++ auth0/v2/blacklists.py
@@ -3,16 +3,15 @@ from .rest import RestClient
 
 class Blacklists(object):
     def __init__(self, domain, jwt_token):
-        url = 'https://%s/api/v2/blacklists/tokens' % domain
-
-        self.client = RestClient(endpoint=url, jwt=jwt_token)
+        self.url = 'https://%s/api/v2/blacklists/tokens' % domain
+        self.client = RestClient(jwt=jwt_token)
 
     def get(self, aud=None):
         params = {
             'aud': aud
         }
 
-        return self.client.get(params=params)
+        return self.client.get(self.url, params=params)
 
     def create(self, jti, aud=''):
-        return self.client.post(data={'jti': jti, 'aud': aud})
+        return self.client.post(self.url, data={'jti': jti, 'aud': aud})
