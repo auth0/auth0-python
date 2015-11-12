53ae5eb46e45d085cf7e9f5fb02e6d84a9502aae
Juan Cabrera <jjcp.91@gmail.com>
2015-11-11 22:33:09 -0300

    Add auth0.py

---
+++ auth0/v2/auth0.py
@@ -0,0 +1,26 @@
+from .connection import Connection
+from .client import Client
+from .device_credentials import DeviceCredentials
+from .blacklists import Blacklists
+from .emails import Emails
+from .jobs import Jobs
+from .rules import Rules
+from .stats import Stats
+from .tickets import Tickets
+from .users import Users
+
+
+class Auth0(object):
+    """Docstring for Auth0. """
+
+    def __init__(self, domain, jwt_token):
+        self.client = Client(domain, jwt_token)
+        self.connection = Connection(domain, jwt_token)
+        self.device_credentials = DeviceCredentials(domain, jwt_token)
+        self.blacklists = Blacklists(domain, jwt_token)
+        self.emails = Emails(domain, jwt_token)
+        self.jobs = Jobs(domain, jwt_token)
+        self.rules = Rules(domain, jwt_token)
+        self.stats = Stats(domain, jwt_token)
+        self.tickets = Tickets(domain, jwt_token)
+        self.users = Users(domain, jwt_token)
