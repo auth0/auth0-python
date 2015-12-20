2c1ffd6abed12de8878ec60021ae16dc9c011975
Juan Cabrera <jjcp.91@gmail.com>
2015-12-20 20:04:30 -0300

    Add docstrings in Link class

--- auth0/v2/authentication/link.py
+++ auth0/v2/authentication/link.py
@@ -3,10 +3,19 @@ from .base import AuthenticationBase
 
 class Link(AuthenticationBase):
 
+    """Link accounts endpoints.
+
+    Args:
+        domain (str): Your auth0 domain (e.g: username.auth0.com)
+    """
+
     def __init__(self, domain):
         self.domain = domain
 
     def unlink(self, access_token, user_id):
+        """Unlink an account.
+        """
+
         return self.post(
             url='https://%s/unlink' % self.domain,
             data={
