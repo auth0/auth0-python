1c4c2083551cdddac3b851d31b240293f1f77d5e
Alessandro Pasotti <elpaso@itopen.it>
2016-04-29 14:23:53 +0200

    Fixes test for tokeninfo

--- auth0/v2/test/authentication/test_users.py
+++ auth0/v2/test/authentication/test_users.py
@@ -27,5 +27,5 @@ class TestUsers(unittest.TestCase):
         mock_post.assert_called_with(
             url='https://my.domain.com/tokeninfo',
             data={'id_token': 'jwtoken'},
-            headers={'Content-Type: application/json'}
+            headers={'Content-Type': 'application/json'}
         )
