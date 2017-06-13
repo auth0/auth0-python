fbdd3b4a71d74962807b2eecf66d48178568e1a3
Annyv2 <Annyv2@users.noreply.github.com>
2017-06-13 11:13:56 +0200

    Create base.py

--- auth0/v3/authentication/base.py
+++ auth0/v3/authentication/base.py
@@ -8,18 +8,19 @@ class AuthenticationBase(object):
     def post(self, url, data={}, headers={}):
         response = requests.post(url=url, data=json.dumps(data),
                                  headers=headers)
-        if url.split('/')[-1] == "change_password" and response.status_code == requests.codes.ok:
-            return response.text
         return self._process_response(response)
 
     def get(self, url, params={}, headers={}):
         return requests.get(url=url, params=params, headers=headers).text
 
     def _process_response(self, response):
-        text = json.loads(response.text) if response.text else {}
-
-        if 'error' in text:
-            raise Auth0Error(status_code=text['error'],
-                             error_code=text['error'],
-                             message=text['error_description'])
+        try:
+            text = json.loads(response.text) if response.text else {}
+        except ValueError:
+            return response.text
+        else:
+            if 'error' in text:
+                raise Auth0Error(status_code=text['error'],
+                                 error_code=text['error'],
+                                 message=text['error_description'])
         return text
