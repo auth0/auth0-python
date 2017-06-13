dfb4fe9aebe0994c4fac9759440f7998454c64eb
Annyv2 <Annyv2@users.noreply.github.com>
2017-06-13 11:28:52 +0200

    Non Json response handling and default value

--- auth0/v3/authentication/base.py
+++ auth0/v3/authentication/base.py
@@ -5,19 +5,22 @@ from ..exceptions import Auth0Error
 
 class AuthenticationBase(object):
 
-    def post(self, url, data={}, headers={}):
+    def post(self, url, data=None, headers=None):
         response = requests.post(url=url, data=json.dumps(data),
                                  headers=headers)
         return self._process_response(response)
 
-    def get(self, url, params={}, headers={}):
+    def get(self, url, params=None, headers=None):
         return requests.get(url=url, params=params, headers=headers).text
 
     def _process_response(self, response):
-        text = json.loads(response.text) if response.text else {}
-
-        if 'error' in text:
-            raise Auth0Error(status_code=text['error'],
-                             error_code=text['error'],
-                             message=text['error_description'])
+        try:
+            text = json.loads(response.text) if response.text else {}
+        except ValueError:
+            return response.text
+        else:
+            if 'error' in text:
+                raise Auth0Error(status_code=text['error'],
+                                 error_code=text['error'],
+                                 message=text['error_description'])
         return text
