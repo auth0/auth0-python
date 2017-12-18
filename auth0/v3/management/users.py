1b3eeff8b7f4ba082d790d3c2d07868c9237e0c7
Maximillian Dornseif <m.dornseif@hudora.de>
2017-12-18 11:20:33 +0100

    Unescaped `user_id` in URL lead to suprises
    `user_id`s can contain characters which are not URL-save like `@`. This patch adds proper escaping so `auth0api.users.get('auth0|user@example.com')` returns an existing user instead of 404.
    
    See also https://community.auth0.com/questions/12952/types-of-user-id-allowed

--- auth0/v3/management/users.py
+++ auth0/v3/management/users.py
@@ -1,3 +1,5 @@
+import urllib
+
 from .rest import RestClient
 
 
@@ -21,7 +23,7 @@ class Users(object):
     def _url(self, id=None):
         url = 'https://%s/api/v2/users' % self.domain
         if id is not None:
-            return url + '/' + id
+            return url + '/' + urllib.quote(id)
         return url
 
     def list(self, page=0, per_page=25, sort=None, connection=None, q=None,
