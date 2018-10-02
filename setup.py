1ab0e1d14fdbbc27217309457c094a9a00d7d83c
hugovk <hugovk@users.noreply.github.com>
2018-10-02 09:24:25 +0300

    Use README.rst as long_description on PyPI

--- setup.py
+++ setup.py
@@ -14,10 +14,15 @@ def find_version():
             raise RuntimeError("Unable to find version string.")
 
 
+with open('README.rst') as f:
+    long_description = f.read()
+
+
 setup(
     name='auth0-python',
     version=find_version(),
     description='Auth0 Python SDK',
+    long_description=long_description,
     author='Auth0',
     author_email='support@auth0.com',
     license='MIT',
