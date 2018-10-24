cd81d3b48940bae29fc943927d61a9788dbf8d88
William Rubel <rubelw@Williams-MBP.localdomain>
2018-10-24 15:46:30 -0500

    Added get custom domain configuration and unit test

--- auth0/v3/test/management/test_custom_domains.py
+++ auth0/v3/test/management/test_custom_domains.py
@@ -16,7 +16,6 @@ class TestCustomDomains(unittest.TestCase):
             'https://domain/api/v2/custom-domains'
         )
 
-
     @mock.patch('auth0.v3.management.custom_domains.RestClient')
     def test_create_new(self, mock_rc):
         mock_instance = mock_rc.return_value
@@ -29,4 +28,12 @@ class TestCustomDomains(unittest.TestCase):
         self.assertEqual('https://domain/api/v2/custom-domains',args[0])
         self.assertEqual(kwargs['data'], {'a': 'b', 'c': 'd','e': 'f'})
 
-       
+    @mock.patch('auth0.v3.management.custom_domains.RestClient')
+    def test_get_domain_by_id(self, mock_rc):
+        mock_instance = mock_rc.return_value
+
+        g = CustomDomains(domain='domain', token='jwttoken')
+        g.get_domain_by_id('an-id')
+
+        mock_instance.get.assert_called_with('https://domain/api/v2/custom-domains/an-id')
+
