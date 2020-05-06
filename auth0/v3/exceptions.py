eb2f588d556567d75971b2c8f373950be7486a8b
Luciano Balmaceda <balmacedaluciano@gmail.com>
2020-05-06 18:15:36 -0300

    simplify the exception class definition

--- auth0/v3/exceptions.py
+++ auth0/v3/exceptions.py
@@ -9,8 +9,4 @@ class Auth0Error(Exception):
 
 
 class TokenValidationError(Exception):
-    def __init__(self, message):
-        self.message = message
-
-    def __str__(self):
-        return self.message
+    pass
