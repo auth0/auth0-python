c46a3ca0fc13ebb06016f5262d09f2b3e2ac6e86
Luciano Balmaceda <balmacedaluciano@gmail.com>
2020-07-07 17:01:16 -0300

    add missing optional properties for the user signup endpoint

--- auth0/v3/test/authentication/test_database.py
+++ auth0/v3/test/authentication/test_database.py
@@ -1,5 +1,7 @@
 import unittest
+
 import mock
+
 from ...authentication.database import Database
 
 
@@ -7,7 +9,6 @@ class TestDatabase(unittest.TestCase):
 
     @mock.patch('auth0.v3.authentication.database.Database.post')
     def test_login(self, mock_post):
-
         d = Database('my.domain.com')
 
         d.login(client_id='cid',
@@ -35,7 +36,6 @@ class TestDatabase(unittest.TestCase):
 
     @mock.patch('auth0.v3.authentication.database.Database.post')
     def test_signup(self, mock_post):
-
         d = Database('my.domain.com')
 
         # using only email and password
@@ -48,16 +48,15 @@ class TestDatabase(unittest.TestCase):
 
         self.assertEqual(args[0], 'https://my.domain.com/dbconnections/signup')
         self.assertEqual(kwargs['data'], {
-                         'client_id': 'cid',
-                         'email': 'a@b.com',
-                         'password': 'pswd',
-                         'connection': 'conn',
-                         'username': None,
-                         'user_metadata': None
-                        })
+            'client_id': 'cid',
+            'email': 'a@b.com',
+            'password': 'pswd',
+            'connection': 'conn',
+            'username': None,
+            'user_metadata': None
+        })
 
-
-        # Using also username and metadata
+        # Using also optional properties
         sample_meta = {
             'hobby': 'surfing',
             'preference': {
@@ -69,23 +68,32 @@ class TestDatabase(unittest.TestCase):
                  password='pswd',
                  connection='conn',
                  username='usr',
-                 user_metadata=sample_meta)
+                 user_metadata=sample_meta,
+                 given_name='john',
+                 family_name='doe',
+                 name='john doe',
+                 nickname='johnny',
+                 picture='avatars.com/john-doe')
 
         args, kwargs = mock_post.call_args
 
         self.assertEqual(args[0], 'https://my.domain.com/dbconnections/signup')
         self.assertEqual(kwargs['data'], {
-                         'client_id': 'cid',
-                         'email': 'a@b.com',
-                         'password': 'pswd',
-                         'connection': 'conn',
-                         'username': 'usr',
-                         'user_metadata': sample_meta
-                        })
+            'client_id': 'cid',
+            'email': 'a@b.com',
+            'password': 'pswd',
+            'connection': 'conn',
+            'username': 'usr',
+            'user_metadata': sample_meta,
+            'given_name': 'john',
+            'family_name': 'doe',
+            'name': 'john doe',
+            'nickname': 'johnny',
+            'picture': 'avatars.com/john-doe',
+        })
 
     @mock.patch('auth0.v3.authentication.database.Database.post')
     def test_change_password(self, mock_post):
-
         d = Database('my.domain.com')
 
         d.change_password(client_id='cid',
