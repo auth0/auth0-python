ee96e9727adb9a02adcd5f474a0f5222a7e17963
Luciano Balmaceda <balmacedaluciano@gmail.com>
2020-07-07 17:01:16 -0300

    add missing optional properties for the user signup endpoint

--- auth0/v3/authentication/database.py
+++ auth0/v3/authentication/database.py
@@ -1,9 +1,9 @@
-from .base import AuthenticationBase
 import warnings
+
+from .base import AuthenticationBase
 
 
 class Database(AuthenticationBase):
-
     """Database & Active Directory / LDAP Authentication.
 
     Args:
@@ -35,10 +35,10 @@ class Database(AuthenticationBase):
             }
         )
 
-    def signup(self, client_id, email, password, connection, username=None,
-             user_metadata=None):
+    def signup(self, client_id, email, password, connection, username=None, user_metadata=None,
+               given_name=None, family_name=None, name=None, nickname=None, picture=None):
         """Signup using email and password.
-        
+
         Args:
            client_id (str): ID of the application to use.
 
@@ -53,6 +53,17 @@ class Database(AuthenticationBase):
            user_metadata (dict, optional): Additional key-value information to store for the user.
                     Some limitations apply, see: https://auth0.com/docs/metadata#metadata-restrictions
 
+           given_name (str, optional): The user's given name(s).
+
+           family_name (str, optional): The user's family name(s).
+
+           name (str, optional): The user's full name.
+
+           nickname (str, optional): The user's nickname.
+
+           picture (str, optional): A URI pointing to the user's picture.
+
+
         See: https://auth0.com/docs/api/authentication#signup
         """
         body = {
@@ -63,6 +74,21 @@ class Database(AuthenticationBase):
             'username': username,
             'user_metadata': user_metadata
         }
+
+        if username:
+            body.update({'username': username})
+        if user_metadata:
+            body.update({'user_metadata': user_metadata})
+        if given_name:
+            body.update({'given_name': given_name})
+        if family_name:
+            body.update({'family_name': family_name})
+        if name:
+            body.update({'name': name})
+        if nickname:
+            body.update({'nickname': nickname})
+        if picture:
+            body.update({'picture': picture})
 
         return self.post(
             'https://{}/dbconnections/signup'.format(self.domain),
