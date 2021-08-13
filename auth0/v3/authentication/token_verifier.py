6341d84da44cc29661614fcb316456d21c43f8a2
Jessica Temporal <jessica.temporal@auth0.com>
2021-08-13 09:55:49 -0300

    Fixes identation warning on TokenVerifier.verify docstrings

--- auth0/v3/authentication/token_verifier.py
+++ auth0/v3/authentication/token_verifier.py
@@ -227,7 +227,7 @@ class TokenVerifier():
             nonce (str, optional): The nonce value sent during authentication.
             max_age (int, optional): The max_age value sent during authentication.
             organization (str, optional): The expected organization ID (org_id) claim value. This should be specified
-                when logging in to an organization.
+            when logging in to an organization.
 
         Raises:
             TokenValidationError: when the token cannot be decoded, the token signing algorithm is not the expected one,
