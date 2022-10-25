60183f377affa368647d754735c6a32088054d26
frederikprijck <frederik.prijck@auth0.com>
2022-10-25 11:56:23 +0200

    revert changes

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -66,8 +66,6 @@ extensions = [
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ["_templates"]
 
-html_extra_path = ["../../auth0_dark_mode.png", "../../auth0_light_mode.png"]
-
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
