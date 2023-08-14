c3ec9acc91ac605abe4f6a162878b98909031658
Evan Sims <hello@evansims.com>
2023-08-14 04:18:24 -0500

    Update test_rest.py

--- auth0/test/management/test_rest.py
+++ auth0/test/management/test_rest.py
@@ -104,31 +104,31 @@ class TestRest(unittest.TestCase):
         self.assertEqual(rc.options.telemetry, True)
 
     def test_get_can_timeout(self):
-        rc = RestClient(jwt="a-token", telemetry=False, timeout=1.0)
+        rc = RestClient(jwt="a-token", telemetry=False, timeout=0)
 
         with self.assertRaises(requests.exceptions.Timeout):
             rc.get("https://google.com")
 
     def test_post_can_timeout(self):
-        rc = RestClient(jwt="a-token", telemetry=False, timeout=1.0)
+        rc = RestClient(jwt="a-token", telemetry=False, timeout=0)
 
         with self.assertRaises(requests.exceptions.Timeout):
             rc.post("https://google.com")
 
     def test_put_can_timeout(self):
-        rc = RestClient(jwt="a-token", telemetry=False, timeout=1.0)
+        rc = RestClient(jwt="a-token", telemetry=False, timeout=0)
 
         with self.assertRaises(requests.exceptions.Timeout):
             rc.put("https://google.com")
 
     def test_patch_can_timeout(self):
-        rc = RestClient(jwt="a-token", telemetry=False, timeout=1.0)
+        rc = RestClient(jwt="a-token", telemetry=False, timeout=0)
 
         with self.assertRaises(requests.exceptions.Timeout):
             rc.patch("https://google.com")
 
     def test_delete_can_timeout(self):
-        rc = RestClient(jwt="a-token", telemetry=False, timeout=1.0)
+        rc = RestClient(jwt="a-token", telemetry=False, timeout=0)
 
         with self.assertRaises(requests.exceptions.Timeout):
             rc.delete("https://google.com")
