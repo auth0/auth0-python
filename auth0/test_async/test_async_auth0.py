e9e7dc3b354394937e6360d339696ddd4b5a49c4
Evan Sims <hello@evansims.com>
2023-08-15 00:44:51 -0500

    Update tests

--- auth0/test_async/test_async_auth0.py
+++ auth0/test_async/test_async_auth0.py
@@ -2,6 +2,7 @@ import re
 import unittest
 from unittest.mock import ANY, MagicMock
 
+import pytest
 from aioresponses import CallbackResult, aioresponses
 from yarl import URL
 
@@ -21,17 +22,18 @@ def get_callback(status=200):
     return callback, mock
 
 
-@unittest.skipIf(
-    not hasattr(unittest, "IsolatedAsyncioTestCase"),
-    "python 3.7 doesn't have IsolatedAsyncioTestCase",
-)
-class TestAuth0(getattr(unittest, "IsolatedAsyncioTestCase", object)):
+class TestAuth0(unittest.TestCase):
+    @pytest.mark.asyncio
     @aioresponses()
     async def test_get(self, mocked):
         callback, mock = get_callback()
-        mocked.get(clients, callback=callback)
+
+        await mocked.get(clients, callback=callback)
+
         auth0 = Auth0(domain="example.com", token="jwt")
+
         self.assertEqual(await auth0.clients.all_async(), payload)
+
         mock.assert_called_with(
             URL("https://example.com/api/v2/clients?include_fields=true"),
             allow_redirects=True,
@@ -40,18 +42,22 @@ class TestAuth0(getattr(unittest, "IsolatedAsyncioTestCase", object)):
             timeout=ANY,
         )
 
+    @pytest.mark.asyncio
     @aioresponses()
     async def test_shared_session(self, mocked):
         callback, mock = get_callback()
         callback2, mock2 = get_callback()
-        mocked.get(clients, callback=callback)
-        mocked.put(factors, callback=callback2)
+
+        await mocked.get(clients, callback=callback)
+        await mocked.put(factors, callback=callback2)
+
         async with Auth0(domain="example.com", token="jwt") as auth0:
             self.assertEqual(await auth0.clients.all_async(), payload)
             self.assertEqual(
                 await auth0.guardian.update_factor_async("factor-1", {"factor": 1}),
                 payload,
             )
+
         mock.assert_called_with(
             URL("https://example.com/api/v2/clients?include_fields=true"),
             allow_redirects=True,
@@ -59,6 +65,7 @@ class TestAuth0(getattr(unittest, "IsolatedAsyncioTestCase", object)):
             headers=ANY,
             timeout=ANY,
         )
+
         mock2.assert_called_with(
             URL("https://example.com/api/v2/guardian/factors/factor-1"),
             allow_redirects=True,
