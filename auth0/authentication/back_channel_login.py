b95cb5a0eba0c19ea17c846ee7a69d4fc1864f8f
Snehil Kishore <snehil.kishore@okta.com>
2025-06-02 20:26:19 +0530

    fix: support `authorization_details`

--- auth0/authentication/back_channel_login.py
+++ auth0/authentication/back_channel_login.py
@@ -1,13 +1,20 @@
-from typing import Any
+from typing import Any, Optional, Union
 
 from .base import AuthenticationBase
+
+import json
 
 
 class BackChannelLogin(AuthenticationBase):
     """Back-Channel Login endpoint"""
 
     def back_channel_login(
-        self, binding_message: str, login_hint: str, scope: str, **kwargs
+        self,
+        binding_message: str,
+        login_hint: str,
+        scope: str,
+        authorization_details:  Optional[Union[str, list[dict]]] = None,
+        **kwargs
     ) -> Any:
         """Send a Back-Channel Login.
 
@@ -15,24 +22,38 @@ class BackChannelLogin(AuthenticationBase):
              binding_message (str): Human-readable string displayed on both the device calling /bc-authorize and the user’s 
              authentication device to ensure the user is approves the correct request.
 
-             login_hint (str): String containing information about the user to contact for authentication.
+             login_hint (str): A JSON object containing user details for authentication in the iss_sub format.
 
              scope(str): "openid" is a required scope.Multiple scopes are separated 
              with whitespace.
 
-             **kwargs: Other fields to send along with the PAR.
+             authorization_details (str, list of dict, or dict, optional): JSON string or dictionary representing
+             Rich Authorization Requests (RAR) details to include in the CIBA request.
+
+             **kwargs: Other fields to send along with the request.
 
         Returns:
             auth_req_id, expires_in, interval
         """
-        return self.authenticated_post(
-            f"{self.protocol}://{self.domain}/bc-authorize",
-            data={
+
+        data = {
                 "client_id": self.client_id,
                 "binding_message": binding_message,
                 "login_hint": login_hint,
                 "scope": scope,
                 **kwargs,
-            },
+            }
+
+        if authorization_details is not None:
+            if isinstance(authorization_details, str):
+                data["authorization_details"] = authorization_details
+            elif isinstance(authorization_details, (list, dict)):
+                data["authorization_details"] = json.dumps(authorization_details)
+                
+        data.update(kwargs)
+
+        return self.authenticated_post(
+            f"{self.protocol}://{self.domain}/bc-authorize",
+            data = data,
             headers={"Content-Type": "application/x-www-form-urlencoded"},
         )
