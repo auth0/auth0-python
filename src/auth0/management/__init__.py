32d0dce99c86a3d5a91a7803797e35f1eedb7848
fern-api <115122769+fern-api[bot]@users.noreply.github.com>
2026-05-26 15:18:36 +0000

    [fern-replay] Applied customizations
    Patches applied (2):
      - patch-4894603d: chore: Restore custom wiring in management/__init__.py
      - patch-7271f157: Revert wrapper file to preserve Auth0 telemetry customizations

--- src/auth0/management/__init__.py
+++ src/auth0/management/__init__.py
@@ -2783,6 +2783,8 @@ if typing.TYPE_CHECKING:
     from .environment import Auth0Environment
     from .event_streams import EventStreamsCreateRequest
     from .version import __version__
+    from .management_client import AsyncManagementClient, CustomDomainHeader, ManagementClient
+    from .token_provider import AsyncTokenProvider, TokenProvider
 _dynamic_imports: typing.Dict[str, str] = {
     "Action": ".types",
     "ActionBase": ".types",
@@ -2843,6 +2845,8 @@ _dynamic_imports: typing.Dict[str, str] = {
     "AssociateOrganizationClientGrantResponseContent": ".types",
     "AsyncApprovalNotificationsChannelsEnum": ".types",
     "AsyncAuth0": ".client",
+    "AsyncManagementClient": ".management_client",
+    "AsyncTokenProvider": ".token_provider",
     "AttackProtectionCaptchaArkoseResponseContent": ".types",
     "AttackProtectionCaptchaAuthChallengeRequest": ".types",
     "AttackProtectionCaptchaAuthChallengeResponseContent": ".types",
@@ -3802,6 +3806,7 @@ _dynamic_imports: typing.Dict[str, str] = {
     "CredentialDeviceTypeEnum": ".types",
     "CredentialId": ".types",
     "CustomDomain": ".types",
+    "CustomDomainHeader": ".management_client",
     "CustomDomainCustomClientIpHeader": ".types",
     "CustomDomainCustomClientIpHeaderEnum": ".types",
     "CustomDomainProvisioningTypeEnum": ".types",
@@ -5073,6 +5078,7 @@ _dynamic_imports: typing.Dict[str, str] = {
     "LogStreamSumoEnum": ".types",
     "LogStreamSumoResponseSchema": ".types",
     "LogStreamSumoSink": ".types",
+    "ManagementClient": ".management_client",
     "MdlPresentationProperties": ".types",
     "MdlPresentationRequest": ".types",
     "MdlPresentationRequestProperties": ".types",
@@ -5318,6 +5324,7 @@ _dynamic_imports: typing.Dict[str, str] = {
     "TokenQuota": ".types",
     "TokenQuotaClientCredentials": ".types",
     "TokenQuotaConfiguration": ".types",
+    "TokenProvider": ".token_provider",
     "TooManyRequestsError": ".errors",
     "TooManyRequestsSchema": ".types",
     "TooManyRequestsSchemaError": ".types",
@@ -5641,6 +5648,8 @@ __all__ = [
     "AssociateOrganizationClientGrantResponseContent",
     "AsyncApprovalNotificationsChannelsEnum",
     "AsyncAuth0",
+    "AsyncManagementClient",
+    "AsyncTokenProvider",
     "AttackProtectionCaptchaArkoseResponseContent",
     "AttackProtectionCaptchaAuthChallengeRequest",
     "AttackProtectionCaptchaAuthChallengeResponseContent",
@@ -7871,6 +7880,7 @@ __all__ = [
     "LogStreamSumoEnum",
     "LogStreamSumoResponseSchema",
     "LogStreamSumoSink",
+    "ManagementClient",
     "MdlPresentationProperties",
     "MdlPresentationRequest",
     "MdlPresentationRequestProperties",
@@ -8113,6 +8123,7 @@ __all__ = [
     "TestEventDataContent",
     "TokenExchangeProfileResponseContent",
     "TokenExchangeProfileTypeEnum",
+    "TokenProvider",
     "TokenQuota",
     "TokenQuotaClientCredentials",
     "TokenQuotaConfiguration",
